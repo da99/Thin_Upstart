c302b83fe134981bf85f7e675021d9f760e2b119
da99 <i-hate-spam-45671204@mailinator.com>
2012-04-15 11:49:06 -0500

    Added: spec for ending slash of dirs.

--- spec/tests/create.rb
+++ spec/tests/create.rb
@@ -1,3 +1,16 @@
+describe "Thin_Upstart settings" do
+  
+  %w{ apps output }.each { |dir|
+    it "removes ending slash of :#{dir}" do
+      target = File.expand_path('.')
+      o = Thin_Upstart.new
+      o.apps "./#{dir}/"
+      o.apps.should == "#{target}/#{dir}"
+    end
+  }
+  
+end # === Thin_Upstart settings
+
 
 describe "Thin_Upstart create" do
   
